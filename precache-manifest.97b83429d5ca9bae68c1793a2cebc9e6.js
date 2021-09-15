470ae2d4c584da07748bf7d0ab7c4edb39829508
Peter Weigman <phweigman@gmail.com>
2021-09-15 18:46:12 -0400

    Updates

---
+++ precache-manifest.97b83429d5ca9bae68c1793a2cebc9e6.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eedae29daef97db8bbcfdc54469bd9fd",
+    "url": "/test-deploy-portfolio/index.html"
+  },
+  {
+    "revision": "475991852a2c6039efec",
+    "url": "/test-deploy-portfolio/static/css/main.883912da.chunk.css"
+  },
+  {
+    "revision": "ec7dea27f2d267470dff",
+    "url": "/test-deploy-portfolio/static/js/2.102548e6.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/test-deploy-portfolio/static/js/2.102548e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "475991852a2c6039efec",
+    "url": "/test-deploy-portfolio/static/js/main.c6f4c7d6.chunk.js"
+  },
+  {
+    "revision": "a6b9e57db7c1a0894558",
+    "url": "/test-deploy-portfolio/static/js/runtime-main.46c3afe4.js"
+  },
+  {
+    "revision": "a19f2b67a0540f784bf091db8e36c82a",
+    "url": "/test-deploy-portfolio/static/media/calculator.a19f2b67.jpg"
+  },
+  {
+    "revision": "402a471b5aad190a33b649f1368f5de1",
+    "url": "/test-deploy-portfolio/static/media/cover-image.402a471b.jpg"
+  },
+  {
+    "revision": "653c2b3d29f66a3c6e957518f3bd16f0",
+    "url": "/test-deploy-portfolio/static/media/led-wall.653c2b3d.jpg"
+  },
+  {
+    "revision": "ed494730868bd902e0ee98f1df1877e2",
+    "url": "/test-deploy-portfolio/static/media/pastel-puzzels.ed494730.jpg"
+  },
+  {
+    "revision": "938f8fe0fe2e5880b3ef528c5188a096",
+    "url": "/test-deploy-portfolio/static/media/run-buddy.938f8fe0.jpg"
+  },
+  {
+    "revision": "12557c9b108cf5e5347f8d838014b31e",
+    "url": "/test-deploy-portfolio/static/media/surf-report.12557c9b.jpg"
+  }
+]);
